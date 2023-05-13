a4a52aa330428d1f7dfdec2bdb61efdb9c673967
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-05-14 01:19:38 +0200

    refactoring

---
+++ deckfm/input.py
@@ -0,0 +1,34 @@
+import os
+
+from kivy.event import EventDispatcher
+from kivy.properties import ListProperty
+
+VDF_PATH = f"{os.path.dirname(__file__)}/input.vdf"
+
+class Input(EventDispatcher):
+    controllers = ListProperty([])
+
+    def __init__(self, steam_input) -> None:
+        self.input = steam_input
+
+        steam_input.Init(True)
+
+        if not steam_input.SetInputActionManifestFilePath(VDF_PATH):
+          raise "cannot load input.vdf"
+
+    def update(self):
+        self.input.RunFrame()
+        self.controllers = self.input.GetConnectedControllers()
+        self._read_input_data()
+
+    def on_controllers(self, _, controllers):
+        handle = self.input.GetActionSetHandle("deckfm")
+
+        for controller in controllers:
+            # TODO: if not set yet
+            self.input.ActivateActionSet(controller, handle)
+
+    def _read_input_data(self):
+        a = self.input.GetDigitalActionHandle("up")
+        for controller in self.controllers:
+            print(self.input.GetDigitalActionData(controller, a).bState)
---
+++ deckfm/main.py
@@ -0,0 +1,42 @@
+import os
+
+from input import Input
+
+os.environ["KIVY_METRICS_DENSITY"] = "2.5"
+
+from kivy.clock import Clock  # noqa: E402
+from kivy.properties import ObjectProperty  # noqa: E402
+from kivymd.app import MDApp  # noqa: E402
+from kivymd.uix.boxlayout import MDBoxLayout  # noqa: E402
+from kivymd.uix.spinner import MDSpinner  # noqa: E402
+from steamworks import STEAMWORKS as SW  # noqa: E402
+
+STEAMWORKS = SW()
+STEAMWORKS.initialize()
+
+class DeckFM(MDBoxLayout):
+    orientation = "vertical"
+    steam = ObjectProperty(STEAMWORKS)
+    input = ObjectProperty(Input(STEAMWORKS.Input))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+        # Required to make steam believe it's game by rendering a few frames after start
+        load_spinner = MDSpinner()
+        self.add_widget(load_spinner)
+        Clock.schedule_once(lambda _: self.remove_widget(load_spinner), 1)
+
+    def update(self, dt):
+        STEAMWORKS.run_callbacks()
+        self.canvas.ask_update()
+        self.input.update()
+
+
+class DeckFMApp(MDApp):
+    pass
+
+
+if __name__ == "__main__":
+    DeckFMApp().run()
